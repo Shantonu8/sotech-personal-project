9f079244441b8b94896dee88f16a5a766828b5bb
Shantonu <shantonupatiil08@gmail.com>
2020-09-15 18:08:04 +0000

    Add about page (change font color)

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const express = require("express"),
 app.set("view engine", "ejs")
 app.use(express.static("public"));
 
-
+// ROUTES
 app.get("/", function(req, res){
 	res.render("landing")
+	console.log("SOMEONE VISITED THE WEBSITE")
 })
 
 app.get("/tech", function(req, res){
@@ -18,6 +19,9 @@ app.get("/media", function(req, res){
 	res.render("media")
 })
 
+app.get("/about-me", function(req, res){
+	res.render("about")
+})
 
 app.listen(3000, function(){
 	console.log("Serving on Port 3000")
