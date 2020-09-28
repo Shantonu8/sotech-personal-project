0a5588602550d281bb817b97b84050d3fb5cdb77
Shantonu <shantonupatiil08@gmail.com>
2020-09-28 11:13:03 +0000

    Before about me

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express"),
 	  app     = express()
 
-
+// SERVER STUFF
 app.set("view engine", "ejs")
 app.use(express.static("public"));
 
-// ROUTES
+//GENERAL ROUTES
 app.get("/", function(req, res){
 	res.render("landing")
 	console.log("SOMEONE VISITED THE WEBSITE")
@@ -26,9 +26,32 @@ app.get("/test", function(req, res){
 	res.render("test")	
 })
 
-app.get("/media/twitter", function(req, res){
-	res.render("twitter")
+//TECH ROUTES
+app.get("/tech/cloud-computing", function(req, res){
+	res.render("cloud")
 })
+app.get("/tech/blockchain", function(req, res){
+	res.render("block")
+})
+app.get("/tech/vr", function(req, res){
+	res.render("vr")
+})
+
+
+// SOCIAL MEDIA ROUTES
+app.get("/media/twitter", function(req, res){
+	res.render("twitter");
+})
+
+app.get("/media/facebook", function(req, res){
+	res.render("facebook");
+})
+
+app.get("/media/instagram", function(req, res){
+	res.render("instagram");
+})
+
+// SERVER STUFF
 
 app.listen(3000, function(){
 	console.log("Serving on Port 3000")
