29b954ff3eb80ebfcca2e37069df7767e47e2df8
Shantonu <shantonupatiil08@gmail.com>
2020-10-06 17:11:34 +0000

    Improve Loading times and image fades

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,38 @@
 
 
 $(document).ready(function(){
+	
+	
+// 	fadein effects
 	$(".fadeIn").animate({opacity: 1}, 1000)
-})
-$(document).ready(function(){
-	$(".fadeIn1").animate({opacity: 1}, 1250)
-})
-$(document).ready(function(){
 	$(".fadeIn2").animate({opacity: 1}, 1500)
+	$(".fadeIn1").animate({opacity: 1}, 1250)
+	
+// Scroll effects 
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 300){
+			$(".imgFade").css({"opacity": "1"})
+		}
+		else {
+			$(".imgFade").css({"opacity": "0"})
+		}
+	})
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 100){
+			$(".scrollFade").css({"opacity": "1"})
+		}
+	})
+	
+	
 })
+// $(document).ready(function(){
+	
+// })
+// $(document).ready(function(){
+	
+// })
+
+
+
+
+
